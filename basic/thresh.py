3442ec91e42982b665ebf495021079b15eb843c2
tomfriwel <976932447@qq.com>
2017-12-28 14:37:28 +0800

    THRESH_BINARY

--- basic/thresh.py
+++ basic/thresh.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 # coding:utf-8
-
+import os
+wk_dir = os.path.dirname(os.path.realpath(__file__))
 import sys
-sys.path.append('../utils')
+sys.path.append(wk_dir+'/../utils')
 
 import cv2
 import util
 
-
-filePath = "../images/figure.jpg"
+filePath = wk_dir+"/../images/figure.jpg"
 img = cv2.imread(filePath, 1)
 
-util.showOpenCVImagesGrid([img], 1, 2, titles=[''])
+thr = cv2.threshold(img, 149, 255, cv2.THRESH_BINARY)[1]
+
+util.showOpenCVImagesGrid([img, thr], 1, 2, titles=['original image', 'THRESH_BINARY'])
 
 
 
